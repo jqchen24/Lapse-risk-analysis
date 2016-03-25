9e6168d446266cbed497f57ee08c72f803189c63
Junqi <jqchen24@gmail.com>
2016-03-25 16:01:44 -0500

    Some more EDA

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -4,3 +4,10 @@ accounts$ ï..account <- NULL
 library(ggplot2)
 ggplot(accounts, aes(RECENCY)) + geom_bar()
 ggplot(accounts, aes(TENURE)) + geom_bar()
+names(accounts)
+table(accounts$Customer_Size)
+table(accounts$CONTRACT_FLAG)
+table(accounts$mro_decile)
+table(accounts$indseg0)
+# 22.7% accounts lapsed in the data.
+table(accounts$churn)/nrow(accounts)
