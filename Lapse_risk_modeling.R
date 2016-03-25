bbbd732fe7b33ed23c595db01736a95a6736ea87
Junqi <jqchen24@gmail.com>
2016-03-25 16:01:04 -0500

    Create some histograms

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -1,3 +1,6 @@
 accounts <- read.csv('accounts_churn_contact_flag.csv', stringsAsFactors = F)
 str(accounts)
 accounts$ ï..account <- NULL
+library(ggplot2)
+ggplot(accounts, aes(RECENCY)) + geom_bar()
+ggplot(accounts, aes(TENURE)) + geom_bar()
