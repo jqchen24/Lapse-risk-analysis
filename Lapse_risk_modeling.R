b24eeb45393137aad7fb4fda1a52ba0fe9f4bd71
Junqi <jqchen24@gmail.com>
2016-03-28 09:15:20 -0500

    Add some comments

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -1,16 +1,26 @@
+##########################################
+# Load the dataset and remove account # #
+##########################################
 accounts <- read.csv('accounts_churn_contact_flag.csv', stringsAsFactors = F)
 str(accounts)
 accounts$ ï..account <- NULL
+
+# Create some histograms
 library(ggplot2)
 ggplot(accounts, aes(RECENCY)) + geom_bar()
 ggplot(accounts, aes(TENURE)) + geom_bar()
+
+# Some EDA
 names(accounts)
 table(accounts$Customer_Size)
 table(accounts$CONTRACT_FLAG)
 table(accounts$mro_decile)
 table(accounts$indseg0)
+
 # 22.7% accounts lapsed in the data.
 table(accounts$churn)/nrow(accounts)
+
+# Check how the churn values are distributed across different categorical variables.
 sort(tapply(accounts$churn, accounts$indseg0, mean), decreasing = T)
 table(accounts$churn, accounts$indseg0)
 sort(tapply(accounts$churn, accounts$Customer_Size, mean), decreasing = T)
@@ -36,13 +46,18 @@ accounts_test <- subset(accounts, spl == F)
 table(accounts_train$churn)/nrow(accounts_train)
 # The distribution of churn in the training data is the same as in original data.
 
+########################################################
+# Baseline model
+########################################################
+table(accounts_test$churn)
+# Baseline model accuracy is 77.3%.
+
+########################################################
 # Build a logistic regression model
+########################################################
 logReg <- glm(churn ~ indseg0 + Customer_Size + INVSOLFLG + mro_decile, data = accounts_train, family = binomial)
 # Evaluate the model
 predict_logReg <- predict(logReg, newdata = accounts_test, type = 'response')
 table(accounts_test$churn, predict_logReg >= 0.5)
 # accuracy is (50828 + 4025)/nrow(accounts_test) = 79.2%
 
-# Baseline model -- predict all the accounts to remain?
-table(accounts_test$churn)
-# Baseline model accuracy is 77.3%.
