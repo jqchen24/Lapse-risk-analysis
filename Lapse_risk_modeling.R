b1ea0f05aefe6971b81bf79a34c68e050465f970
Junqi <jqchen24@gmail.com>
2016-03-28 15:41:23 -0500

    Added discount, but the model wouldn't work. Need to deal with missing vlaues.

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -40,6 +40,7 @@ sort(tapply(accounts$churn, accounts$contract_group, mean), decreasing = T)
 # Feature engineering
 #######################################################
 accounts$discount = (accounts$WA_S12X - (accounts$SALES12X - accounts$FINDS12X))/accounts$WA_S12X
+ggplot(accounts, aes(discount)) + geom_bar()
 
 # split the dataset to training/test
 library(caTools)
@@ -59,7 +60,7 @@ table(accounts_test$churn)
 ########################################################
 # Build a logistic regression model
 ########################################################
-logReg <- glm(churn ~ CONTACTS + TENURE + TRANS12X + LINES12X + CONTRACT_FLAG + SOW, data = accounts_train, family = binomial)
+logReg <- glm(churn ~ CONTACTS + TENURE + TRANS12X + LINES12X + CONTRACT_FLAG + discount, data = accounts_train, family = binomial)
 
 # Evaluate the model
 library(caret)
