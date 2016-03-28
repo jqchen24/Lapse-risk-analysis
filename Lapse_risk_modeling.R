0188a54b6923be88126a63ae82b55b1735362f26
Junqi <jqchen24@gmail.com>
2016-03-28 10:02:26 -0500

    Include recency, tenure and sales12x, accuracy improved.

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -54,9 +54,9 @@ table(accounts_test$churn)
 ########################################################
 # Build a logistic regression model
 ########################################################
-logReg <- glm(churn ~ indseg0 + Customer_Size + INVSOLFLG + mro_decile + contract_group, data = accounts_train, family = binomial)
+logReg <- glm(churn ~ RECENCY + TENURE + SALES12X + indseg0 + Customer_Size + INVSOLFLG + mro_decile + contract_group, data = accounts_train, family = binomial)
 # Evaluate the model
 predict_logReg <- predict(logReg, newdata = accounts_test, type = 'response')
 table(accounts_test$churn, predict_logReg >= 0.5)
-# accuracy is (50482 +  4691)/nrow(accounts_test) = 79.65%
+# accuracy is (50287 +  5755)/nrow(accounts_test) = 80.9%
 
