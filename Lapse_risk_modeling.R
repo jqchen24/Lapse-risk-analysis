6357920d83696ca579200bd81b5000a9c8f24f50
Junqi <jqchen24@gmail.com>
2016-03-29 14:00:19 -0500

    Convert sellertype to factor and include in the model, accuracy and AUC increased, sensitivity dropped.

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -45,6 +45,7 @@ accounts <- mutate(accounts, discount = (WA_S12X - (SALES12X - FINDS12X))/WA_S12
 ggplot(accounts, aes(discount)) + geom_bar()
 # CSG
 accounts <- mutate(accounts, sellertype = CSG %/% 10000)
+accounts$sellertype <- as.factor(accounts$sellertype)
 
 # split the dataset to training/test
 library(caTools)
@@ -64,7 +65,7 @@ table(accounts_test$churn)
 ########################################################
 # Build a logistic regression model
 ########################################################
-logReg <- glm(churn ~ CONTACTS + TENURE + log(TRANS12X) + LINES12X  + indseg1 + contract_group + log(mrospend), data = accounts_train, family = binomial)
+logReg <- glm(churn ~ CONTACTS + TENURE + log(TRANS12X) + LINES12X  + indseg1 + contract_group + log(mrospend) + sellertype, data = accounts_train, family = binomial)
 summary(logReg)
 # All variables are significant.
 
@@ -73,12 +74,12 @@ library(caret)
 predict_logReg <- predict(logReg, newdata = accounts_test, type = 'response')
 # Note that both arguments in the confusionMatrix have to have the same values (either T/F or 0/1)
 confusionMatrix(predict_logReg >= 0.5, accounts_test$churn==1)
-# accuracy is 84.08%
-# Sensitivity is 92.68%
+# accuracy is 84.09%
+# Sensitivity is 92.60%
 ## May prefer models with higher overall accuracy but also lower false negative, thus higher sensitivity.
 
 
-# Calculate AUC value and generate the ROC curve. AUC = 0.8903135
+# Calculate AUC value and generate the ROC curve. AUC = 0.8905507
 library(ROCR)
 ROCRpred <- prediction(predict_logReg, accounts_test$churn)
 as.numeric(performance(ROCRpred, "auc")@y.values)
