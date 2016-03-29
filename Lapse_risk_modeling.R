d6b9edcffc9f8b56cf7417d9688d640180b4b952
Junqi <jqchen24@gmail.com>
2016-03-29 10:43:56 -0500

    Remove IVSLN12X from the interaction term, accuracy and AUC dropped a tiny.

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -61,7 +61,7 @@ table(accounts_test$churn)
 ########################################################
 # Build a logistic regression model
 ########################################################
-logReg <- glm(churn ~ CONTACTS + TENURE + TRANS12X + LINES12X + CONTRACT_FLAG + INVSOLFLG:IVSLN12X
+logReg <- glm(churn ~ CONTACTS + TENURE + TRANS12X + LINES12X + CONTRACT_FLAG + INVSOLFLG
               , data = accounts_train, family = binomial)
 summary(logReg)
 # All variables are significant.
@@ -71,12 +71,12 @@ library(caret)
 predict_logReg <- predict(logReg, newdata = accounts_test, type = 'response')
 # Note that both arguments in the confusionMatrix have to have the same values (either T/F or 0/1)
 confusionMatrix(predict_logReg >= 0.5, accounts_test$churn==1)
-# accuracy is 83.88%
-# Sensitivity is 90.87%
+# accuracy is 83.87%
+# Sensitivity is 90.88%
 ## May prefer models with higher overall accuracy but also lower false negative, thus higher sensitivity.
 
 
-# Calculate AUC value and generate the ROC curve. AUC = 0.8889513
+# Calculate AUC value and generate the ROC curve. AUC = 0.8889254
 library(ROCR)
 ROCRpred <- prediction(predict_logReg, accounts_test$churn)
 as.numeric(performance(ROCRpred, "auc")@y.values)
