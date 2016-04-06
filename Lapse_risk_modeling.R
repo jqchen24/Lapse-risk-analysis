e53decd6ef930b8d46eaa4651860c4748b6e2b51
Junqi <jqchen24@gmail.com>
2016-04-06 16:27:29 -0500

    Add EPEDN12X, training: ROC +, Sens -, Test: Accuracy -, Sens +, AUC +

--- Lapse_risk_modeling.R
+++ Lapse_risk_modeling.R
@@ -232,7 +232,7 @@ levels(training$churn) <- c("No", "Yes")
 # testTransformed <- predict(preProcValues, testing)
 set.seed(80)
 logReg_caret <- train(churn ~ DISTANCE + CREDIT + CONTACTS + RECENCY + RET_T12 + TENURE + log(TRANS12X) + log(TRANS24X + 1) + LINES12X  + indseg1 + mrospend + 
-                        contract_group + sellertype, 
+                        contract_group + sellertype + EPEDN12X, 
                       data = training, 
                       method = "glm", 
                       metric = "ROC",
@@ -240,8 +240,8 @@ logReg_caret <- train(churn ~ DISTANCE + CREDIT + CONTACTS + RECENCY + RET_T12 +
                       family = binomial)
 logReg_caret
 varImp(logReg_caret)
-# ROC = 0.8931415
-# Sensitivity = 0.9244578
+# ROC = 0.893337
+# Sensitivity = 0.9242878
 summary(logReg_caret)
 # Note that for predict.train under caret, type argument can only be "raw" or "prob"
 # Also note that pred actually is a two column data frame.
@@ -249,14 +249,14 @@ pred <- predict(logReg_caret, newdata = testing, type = "prob")
 confusionMatrix(pred[, 2] >= 0.5, testing$churn == 1, positive = "TRUE")
 ## Need to exclude the missing values in distance, otherwise confusionMatrix won't work.
 confusionMatrix(pred[, 2] >= 0.5, testing[is.na(testing$DISTANCE) != T,]$churn == 1, positive = "TRUE")
-## Accuracy = 84.26%
+## Accuracy = 84.25%
 ## Kappa = 52.04%
-## Sensitivity = 0.5621
+## Sensitivity = 0.5625
 library(ROCR)
 ROCRpred <- prediction(pred[,2], testing$churn)
 ROCRpred <- prediction(pred[,2], testing[is.na(testing$DISTANCE) != T,]$churn)
 as.numeric(performance(ROCRpred, "auc")@y.values)
-# AUC value = 0.8936907
+# AUC value = 0.8938789
 perf <- performance(ROCRpred, "tpr", "fpr")
 plot(perf)
 plot(perf, colorize=T)
